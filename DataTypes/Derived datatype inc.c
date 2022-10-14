5c28ab27b241285d6adea974f6401d047871c77b
Mahima Bhardwaj <90374311+YOGINIMAHIMA1@users.noreply.github.com>
2022-10-14 08:54:14 -0700

    Update Derived datatype inc.c
    modifiers in c
    - short - 2 bytes
    -long-4 bytes
    signed-+ve ,-ve
    unsigned-  -ve

--- DataTypes/Derived datatype inc.c
+++ DataTypes/Derived datatype inc.c
@@ -11,7 +11,7 @@ int main() // main function declaration
   int A[]={1,2,3};  //array declaration
   int _a=22;
   int *p;   // pointer declaration  
-  *p=&_a;      //refrence
+  *p= &_a;      //refrence
   for(int i=0;i<*p;i++)
   {
       int sum=0;
