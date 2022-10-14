f73c53fedd7067c7c348a234e9a6b37439cc2328
Mahima Bhardwaj <90374311+YOGINIMAHIMA1@users.noreply.github.com>
2022-10-14 08:14:15 -0700

    Update primitive-inbuilt.c

--- DataTypes/primitive-inbuilt.c
+++ DataTypes/primitive-inbuilt.c
@@ -6,9 +6,9 @@ int main()
   float b__=2.12;
   bool c_1=0;
   double _d_ =12.22;
-  printf("%n\n ",_);
-  printf("%d\n",__);
-  printf("%f\n",b__);
+  printf("%d\n ",_);
+  printf("%c\n",__);
+  printf("%d\n",b__);
   printf("%f\n",c_1);
   printf("%f\n",_d_);
   return 0;
