cf2fb8caf7eab5dc0507cc5fb680c8ccd0c06403
Mahima Bhardwaj <90374311+YOGINIMAHIMA1@users.noreply.github.com>
2022-10-14 09:26:19 -0700

    Create sizeof().c

---
+++ DataTypes/sizeof().c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main()
+{
+   int _=11;
+  char __='M';
+  float a12_ =22.22;
+  double b0_2_2=90.90;
+  _Bool A_11=1;
+  
+  printf("%d\n", sizeof(_) );
+  printf("%c\n",sizeof(M) );
+  printf("%f\n",sizeof(22.22));
+  printf("%e\n",sizeof(b0_2_2) );
+  printf("%c\n",sizeof(A_11) );
+  
+  return 0;
+}
