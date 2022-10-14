8b88b5b583e259f45263df7903763ae69be73884
Mahima Bhardwaj <90374311+YOGINIMAHIMA1@users.noreply.github.com>
2022-10-14 09:46:34 -0700

    Create variable.c

---
+++ DataTypes/variable.c
@@ -0,0 +1,42 @@
+#include<stdio.h>
+int main()
+{
+  int a=11;
+  short int b=22;
+  unsigned short int c=32;
+  unsigned int d=21;
+  long int f=12;
+   unsigned long int h=1;
+  long long int i=99;
+  unsigned long long int k=111;
+  unsigned char m ='d';
+  long double w= 22.12;
+  wchar_t o= 22;
+  
+  
+  printf("%d\n",c);
+  
+  printf("%u\n",d);
+  
+  printf("%ld\n",f);
+  
+  printf("%e\n",w);
+  
+  printf("%d\n",o);
+  
+  printf("%lu\n",h);
+  
+  printf("%d\n",i);
+  
+  printf("%d\n",k);
+  
+  printf("%c\n",m);
+  
+  
+  
+  printf("%hi\n",b);
+  
+  printf("%c\n",o);
+  
+  
+  
