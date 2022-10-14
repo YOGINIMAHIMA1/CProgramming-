714b35d83939c8d8bbcdfcfc260d074efdf363ab
Mahima Bhardwaj <90374311+YOGINIMAHIMA1@users.noreply.github.com>
2022-10-14 04:14:11 -0700

    primitivedatatype
    -int
    -char
    -float
    -double
    -bool

---
+++ DataTypes/primitive-inbuilt.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+  int _=11;
+  char __='a';
+  float b__=2.12;
+  bool c_1=0;
+  double _d_ =12.22;
+  printf("%n\n ",_);
+  printf("%d\n",__);
+  printf("%f\n",b__);
+  printf("%f\n",c_1);
+  printf("%f\n",_d_);
+  return 0;
+}
+  
