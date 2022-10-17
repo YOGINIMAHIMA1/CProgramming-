ad99772c0bbeaabc5b5553d811c35a18a23142b9
Mahima Bhardwaj <90374311+YOGINIMAHIMA1@users.noreply.github.com>
2022-10-17 08:01:54 -0700

    Update modulusfloat.c
    modulus operator perform operation on integer type
    both operand should be integer
    floating type is not allowed in modulus operator

--- modulusfloat.c
+++ modulusfloat.c
@@ -2,8 +2,11 @@
 int main()
 {
   float a=22.2;
-  a=a%2==0;
+  a=a%2==0; //modulus operator-is used to obtain remainder after performing integer division
   printf("%f",a);
   return 0;
 }
-/* o/p- 11.6 */
+/* o/p-   compile-time error 
+modulus operator does not perform operation on floating point operand.
+  it only perform operation on integer both operand should be integer type
+  for ex- 3%2=1 */
