79dbf2ae2766934af7c58204752f85b52bebdd3d
Mahima Bhardwaj <90374311+YOGINIMAHIMA1@users.noreply.github.com>
2022-10-21 03:52:55 -0700

    Update modify4.c

--- operators/modify4.c
+++ operators/modify4.c
@@ -6,3 +6,9 @@ int main()
   printf("%d/n",a);
   return 0;
 }
+/* modify operator has higher priority than arthmetic operator so first modify operator execute. than arthmetic operator.
+a= a++ +a;
+a=7+7=14;//postfix increment happen
+a=15;   
+
+*/
