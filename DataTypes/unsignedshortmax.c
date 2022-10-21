5701beee0c32576b25171e7ac0166eb0896177b3
Mahima Bhardwaj <90374311+YOGINIMAHIMA1@users.noreply.github.com>
2022-10-21 07:46:22 -0700

    Update unsignedshortmax.c

--- DataTypes/unsignedshortmax.c
+++ DataTypes/unsignedshortmax.c
@@ -4,6 +4,13 @@ int main()
 {
   printf("%u",USHRT_MAX);
   printf("%d",SHRT_MAX);
-  PRINTF("%d",SHRT_MIN);
+  Print("%d",SHRT_MIN);
   return 0;
 }
+/*  limits.h- it is header file provides all constant variables.
+          these variables are represented using capital letters.
+          signed- +ve and -ve -  -32767 to +32768
+          unsigned- +ve - 0 to 65535
+          short= 2 bytes= 2^16=65535
+          format specifier for signed =%d
+          unsigned=%u  */
