a605fde868fc0fbdc43a644ae0fbbb0eddd5375d
Mahima Bhardwaj <90374311+YOGINIMAHIMA1@users.noreply.github.com>
2022-10-21 04:13:47 -0700

    Update whileloopternary.c
    there is difference in i=i+1 and i+1.
    while loop execute till condition is true value i.e i=1,2,34... so on true value when i value is zero in condition thwe loop terminate.

--- loopcase/whileloopternary.c
+++ loopcase/whileloopternary.c
@@ -2,9 +2,15 @@
 int main()
 {
   int i=4,j=2;
-  while(i+1?++i:j--)
+  while(i+1?--i:--j)
   {
     printf("%d\n",i);
     }
   return 0;
 }
+/* i=2,j=2
+while(i+1?--i:--j)
+   while(2+1=3?--i=1(--2)) //i=1
+   
+   while( 1+1=2?--i=(--1) //i=0 while(0) false value generate loop terminate
+   o/p-1 */
