54011cf55071ac6c588515f6863e4a2706301ff1
Mahima Bhardwaj <90374311+YOGINIMAHIMA1@users.noreply.github.com>
2022-10-23 11:01:45 -0700

    Create print 100 elements of array.c

---
+++ arrays/print 100 elements of array.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main ()
+{
+  int n;
+  cout<<"enter the value of n:;
+    cin>>n;
+  int arr[100];
+  
+    for(int i=0;i<n;i++)
+    {
+      cout<<arr[i];
+    }
+    return 0;
+  }
