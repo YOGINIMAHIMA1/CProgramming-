06aad2f1d491ec820a5ac7159d7f3f37ca8a25cd
Mahima Bhardwaj <90374311+YOGINIMAHIMA1@users.noreply.github.com>
2022-10-22 20:39:20 -0700

    Create fun.c

---
+++ functions/fun.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{
+  void fun(int)
+    int main()
+  {
+    
+    int x=3;
+    void fun(x++);
+    return 0;
+  }
+   void fun(int x)
+   {
+     printf("%d\n",x++);
+   }
