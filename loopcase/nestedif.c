e2efe2c9070aa8580161cc0d1325167443d7c2bd
Mahima Bhardwaj <90374311+YOGINIMAHIMA1@users.noreply.github.com>
2022-10-22 20:22:32 -0700

    Create nestedif.c
    nestedif- if block containing one another if block inside it.

---
+++ loopcase/nestedif.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main()
+{
+  int a=3, b=4, c=5;
+  if(a>b)
+  {
+    if(a>c)
+    {
+      printf("Hi\n");
+    }
+      else
+      {
+        printf("Hello\n");
+      }
+    return 0;
+    
+  }
