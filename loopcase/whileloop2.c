72196e113c499edeb818ab136c0e76c9dc4f27c3
Mahima Bhardwaj <90374311+YOGINIMAHIMA1@users.noreply.github.com>
2022-11-02 22:03:18 +0530

    Create whileloop2.c

---
+++ loopcase/whileloop2.c
@@ -0,0 +1,19 @@
+/*
+while(condition)
+{
+   statement;
+   }
+   */
+ #include <stdio.h>
+ int main()
+ {
+   int n;
+   cin>>n;
+   int i=0;
+   while(i<=n)
+   {
+     cout<<i;
+   }
+    i++;
+   return 0;
+ }
