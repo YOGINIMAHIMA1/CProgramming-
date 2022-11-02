bb0581b86dc6347d7c0e30cc21eb1b1171dd8695
Mahima Bhardwaj <90374311+YOGINIMAHIMA1@users.noreply.github.com>
2022-11-02 22:06:17 +0530

    Create dowhile.c

---
+++ loopcase/dowhile.c
@@ -0,0 +1,17 @@
+/*
+do
+{
+    statemnet;
+    }
+    while(condition);
+    */
+#include <stdio.h>
+int main()
+{
+  int i=2;
+  do
+  {
+    cout<<"MAHIMA BHARDWAJ" <<ENDL;
+  }while(i>0) ;
+  return 0;
+}
