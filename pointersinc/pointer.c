1d77aa866022cf2a3b9b69d9f7e831c79339166a
Mahima Bhardwaj <90374311+YOGINIMAHIMA1@users.noreply.github.com>
2022-11-05 23:11:45 +0530

    Update pointer.c
    &i ------ address opeartor -stores the memory address of i
    *ptr ----- pointer operator- store the value at memory address of ptr

--- pointersinc/pointer.c
+++ pointersinc/pointer.c
@@ -5,11 +5,11 @@ void main() //  main method
   int i=100; // datatype identifier   value
   int* ptr; //pointer declaration- it work is to store information from memory location
   ptr= &i;  // address operator - it stores the memory address of ptr variable
-  printf("%d",i);
-    printf("%d",&i);
-  printf("%d",&ptr);
-  printf("%d", *ptr);
-  printf("%d", *(&i));
+  printf("%d",i);// variable i value print at console
+    printf("%d",&i);//print memory address of i
+  printf("%d",&ptr);// print memory address of ptr
+  printf("%d", *ptr); // print value at memory address of variable ptr
+  printf("%d", *(&i)); // it print the value at memory address at i
   return 0;
 }
   
