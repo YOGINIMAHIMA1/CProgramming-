b7cd9996f542540ff8ce1ca1673ae6b54d86318a
Mahima Bhardwaj <90374311+YOGINIMAHIMA1@users.noreply.github.com>
2022-11-06 10:00:04 +0530

    Create arraymaxandmin.c

---
+++ arrays/arraymaxandmin.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main()
+{
+  int n,max,min,ele;
+    scanf("%d",&n);
+  for(int i=1;i<=n;i++)
+  {
+     scanf(%d \n",&ele);
+           }
+           for(int i=1;i<=n;i++)
+           {
+             if(ele[i]>max)
+             {
+               max=ele[i];
+             }
+             if(le[i]<
