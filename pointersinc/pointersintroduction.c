54128952f287437cf856e67e115001a79cab987c
Mahima Bhardwaj <90374311+YOGINIMAHIMA1@users.noreply.github.com>
2022-11-14 09:27:15 +0530

    Create pointersintroduction.c

---
+++ pointersinc/pointersintroduction.c
@@ -0,0 +1,13 @@
+/* Pointers- It is a special variable which stores the starting address of another variable.
+Address- it is positive integer value.
+
+syntax-
+ datatype* identifier; or  datatype *identifier;
+ int* a; or int  *a;
+ 
+                  *a  ----------------- a= pointer ( a stores starting address of another variable)
+                  \
+                   \
+                    \
+                     \      
+                      *a  alias(new name created given to another variable)
