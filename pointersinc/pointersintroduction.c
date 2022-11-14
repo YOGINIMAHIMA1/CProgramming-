a45a2a6011be849013bfaa8c320bbd48e3eeddf6
Mahima Bhardwaj <90374311+YOGINIMAHIMA1@users.noreply.github.com>
2022-11-14 09:34:03 +0530

    Update pointersintroduction.c

--- pointersinc/pointersintroduction.c
+++ pointersinc/pointersintroduction.c
@@ -10,4 +10,18 @@ syntax-
                    \
                     \
                      \      
-                      *a  alias(new name created given to another variable)
+                      *a  alias(new name created given to another variable---- it stores value of variable which has given this new name) */
+
+#include <stdio.h>
+int main()
+{
+  int a, *pa;
+  float b,*pb;
+   pa= &a;
+  pb=&b;
+scanf("%d",&a);
+ scanf("%f",&b);
+ printf("%d",*pa);
+ printf("%d",*pb);
+ return 0;
+}
